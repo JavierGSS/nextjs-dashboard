2b75b578d4b77701f4a3fac321455ea45774a034
Baba <jagasal@icloud.com>
2024-03-19 22:08:10 -0600

    add call to DB

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -1,6 +1,7 @@
 'use server';
 
 import { z } from "zod";
+import { sql } from "@vercel/postgres";
 
 const FormSchema = z.object({
     id: z.string(),
@@ -10,7 +11,7 @@ const FormSchema = z.object({
     date: z.string()
 })
 
-const CreateInvoice = FormSchema.omit({id: true});
+const CreateInvoice = FormSchema.omit({id: true, date: true});
 
 export async function createInvoice(formData: FormData) {
     const {customerId, amount, status} = CreateInvoice.parse({
@@ -18,5 +19,12 @@ export async function createInvoice(formData: FormData) {
         amount: formData.get('amount'),
         status: formData.get('status'),
       });
+      const amountInCents = amount *100;
+      const date = new Date().toISOString().split('T')[0];
+
+      await sql`
+        INSERT INTO invoices (customer_id, amount, status, date)
+        VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
+      `
       
 }
