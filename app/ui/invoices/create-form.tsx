6a2e5f439dbbebbc9d40ddf191322d2feb74c85b
Baba <jagasal@icloud.com>
2024-03-22 20:14:16 -0600

    add revalidation & redirection to actions.ts after creating an invoice

--- app/ui/invoices/create-form.tsx
+++ app/ui/invoices/create-form.tsx
@@ -11,6 +11,7 @@ import { createInvoice } from '@/app/lib/actions';
 
 export default function Form({ customers }: { customers: CustomerField[] }) {
   return (
+    // We call a Server Action defined in actions.ts
     <form action={createInvoice}>
       <div className="rounded-md bg-gray-50 p-4 md:p-6">
         {/* Customer Name */}
