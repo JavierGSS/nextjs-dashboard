a8a8fa699d39c354aa63b4e152610b47314dfecf
Baba <jagasal@icloud.com>
2024-04-01 15:55:00 -0600

    add template to metadata object in /app/layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,10 @@ import { inter } from '@/app/ui/fonts';
 import { Metadata } from 'next';
 
 export const metadata: Metadata = {
-  title: 'Acme Dashboard',
+  title: {
+    template: '%s | Acme Dashboard',
+    default: 'Acme Dashboard',
+  },
   description: 'Official course dashboard w/h app router',
   metadataBase: new URL('https://next-learn-dashboard.vercel.sh'),
 };
